af2e4140c3ac8a13546d644fb455e45c85196eed
Peter McDermott <mcdermpete@gmail.com>
2019-08-20 11:47:24 +1200

    Made ball bounce

---
+++ New Unity Project/Assets/_Scripts/PaddleInput.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleInput : MonoBehaviour
+{
+    public float speed;
+    public float clampValue;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePaddle(Input.GetAxis("Vertical"));
+
+    }
+
+    public void MovePaddle(float inputAxisInfo)
+    {
+        Vector3 clampedGameObjectPosition = gameObject.transform.position + new Vector3(0, 0,inputAxisInfo * speed);
+
+        clampedGameObjectPosition.z = Mathf.Clamp(clampedGameObjectPosition.z, -clampValue, clampValue);
+
+        gameObject.transform.position = clampedGameObjectPosition;
+        
+        
+    }
+
+
+}
